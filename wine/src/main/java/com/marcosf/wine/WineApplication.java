fd224ef77373d8e68e2a29c602881af690863900
marcosf <marcosf@192.168.1.8>
2016-08-03 18:14:52 -0300

    Configuração do Ambiente Finalizada.

--- wine/src/main/java/com/marcosf/wine/WineApplication.java
+++ wine/src/main/java/com/marcosf/wine/WineApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WineApplication {
 //git@github.com:marcosflavio/wine.git
+	
 	public static void main(String[] args) {
 		SpringApplication.run(WineApplication.class, args);
 	}
