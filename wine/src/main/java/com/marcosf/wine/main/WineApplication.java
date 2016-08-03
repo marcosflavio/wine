452f241a2827d3239b32d6c6bf673d0dba8d6cf7
marcosf <marcosf@192.168.1.8>
2016-08-03 18:45:26 -0300

    Second

---
+++ wine/src/main/java/com/marcosf/wine/main/WineApplication.java
@@ -0,0 +1,13 @@
+package com.marcosf.wine.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WineApplication {
+//git@github.com:marcosflavio/wine.git
+	
+	public static void main(String[] args) {
+		SpringApplication.run(WineApplication.class, args);
+	}
+}
